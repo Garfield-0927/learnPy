5ef878fc3574170f9982fa7f3603d78a0fca9729
Garfield_zhang <929780650@qq.com>
2020-10-13 21:39:16 +0800

    first commit

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+# 输出语句
+# print("hello world")
+
+# 输入语句
+# name = input("input your name:")
+# print(name)
+
+# while print fibonacci
+# format: while var < 100 :
+# notice there is a space after 100
+# notice there is a tab in line 14
+# a=b=1
+# while a < 1000 :
+#     print(b,end=",")
+#     temp=b
+#     b=a+b
+#     a=temp
+
+
+# turtle model
+# import turtle
+# turtle.pensize(2)   # 设置画笔宽度为2px
+# turtle.circle(10)   # 绘画半径为10px的circle
+# turtle.circle(40)   # 绘画半径为40px的circle
+# turtle.circle(80)   # 绘画半径为80px的circle
+
+# date
+# from datetime import datetime
+# now = datetime.now()    # get time
+# print(now)
+# print(now.strftime("%x"))  # 输出日期部分
+# print(now.strftime("%X"))  # 输出时间部分
